fd73ee75c16be8c40325ae8d330fe87a99278ee6
Kengston <danildc@mail.ru>
2021-04-06 02:53:02 +0300

    Mark-star update

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,24 +1,41 @@
-import React from 'react';
-
+import React, {Component, useState} from 'react';
+import ReactDOM from 'react-dom'
 import './post-list-item.css';
 
-const PostListItem = () => {
-    return (
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Hello World
-            </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button className="btn-star btn-sm">
-                    <i className="fas fa-star" />
-                </button>
-                <button className="btn-trash btn-sm">
-                    <i className="fas fa-trash-alt" />
-                </button>
-                <i className="fas fa-heart" />
-            </div>
-        </li>
-    )
+const PostListItem = (props) => {
+
+        let [marked, setStar] = useState(true)
+
+        const markPost = () => {
+            setStar(true)
+        }
+
+        const unmarkPost = () => {
+            setStar(false)
+        }
+
+        return (
+            <li className="app-list-item d-flex justify-content-between">
+                <span className="app-list-item-label">
+                    Hello World
+                </span>
+                <div className="d-flex justify-content-center align-items-center">
+                    {marked ?
+                        <button className="btn-star btn-sm" onClick={markPost}>
+                            <i className="far fa-star" />
+                        </button>
+                        :
+                        <button className="btn-star btn-sm" onClick={unmarkPost}>
+                            <i className="fas fa-star"/>
+                        </button>
+                    }
+                    <button className="btn-trash btn-sm">
+                        <i className="fas fa-trash-alt" />
+                    </button>
+                    <i className="fas fa-heart" />
+                </div>
+            </li>
+        )
 }
 
 export default PostListItem;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './components/app/';
 import 'bootstrap/dist/css/bootstrap.css'
 
+
+
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 
